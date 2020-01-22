835ed1b219ac98f8150b3fdf1939e16909a6d03d
EzzPrograms <ezzuddeanprogrammer@gmail.com>
2020-01-22 08:38:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -147,7 +147,7 @@ def gbfull(update, context):
     parse_mode=telegram.ParseMode.MARKDOWN
   )
   time.sleep(0.7)
-  context.bot.send_message(chat_id=update.effective_chat.id, text="Green Balls Full Story", reply_to_message_id=query.message.chat_id)
+  context.bot.send_message(chat_id=update.effective_chat.id, text="Green Balls Full Story", reply_to_message_id=query.message.message_id)
   time.sleep(1)
   context.bot.send_message(chat_id=update.effective_chat.id, text="A long long time ago, in a land far far away, there was this young boy. He was talented and excelled at everything he did, often scoring extremely well for his tests.")
   time.sleep(0.7)
